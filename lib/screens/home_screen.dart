45e50ea320a24b0558c08929c110df7e0587a602
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2023-01-09 22:20:23 -0500

    UPDATE: Theme change implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../share_preferences/preferences.dart';
+import '../providers/theme_provider.dart';
+
+class HomeScreen extends StatefulWidget {
   static const String routerHome = 'Home';
 
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        leading: IconButton(
+          icon: Icon(Preferences.isDarkmode == true
+            ? Icons.light_mode_outlined
+            : Icons.dark_mode_outlined),
+          onPressed: () {
+            Preferences.isDarkmode = !Preferences.isDarkmode;
+            final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+
+            Preferences.isDarkmode
+              ? themeProvider.setDarkmode()
+              : themeProvider.setLightmode();
+            setState(() {});
+          },
+        ),
       ),
       body: const Center(
         child: Text('HomeScreen'),
