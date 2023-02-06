c828feb16983bdf5de17792705a3f3e1fb23a2b9
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2023-02-06 14:26:38 -0500

    ADD: providers.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'screens/screens.dart';
-import 'providers/theme_provider.dart';
+import 'providers/providers.dart';
 import 'share_preferences/preferences.dart';
 
 void main() async {
